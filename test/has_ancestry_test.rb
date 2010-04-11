2e3666b6391c01b5585f9280220e1c0febb35c28
logicaltext <logicaltext@logicaltext.com>
2010-04-11 17:05:50 -0400

    Make tests run independently of a Rails app

--- test/has_ancestry_test.rb
+++ test/has_ancestry_test.rb
@@ -1,17 +1,59 @@
-require File.dirname(__FILE__) + '/test_helper.rb'
+require 'test/unit'
+require 'rubygems'
+require 'active_record'
+require 'ancestry'
 
-# Setup the required models for all test cases
+ActiveRecord::Base.establish_connection :adapter  => "sqlite3",
+                                        :database => ":memory:"
+
+def setup_db
+
+  # AR keeps printing annoying schema statements
+  $stdout_orig = $stdout
+  $stdout = StringIO.new
+
+  ActiveRecord::Base.logger
+  ActiveRecord::Schema.define(:version => 0) do
+    create_table :test_nodes do |t|
+      t.string :ancestry
+      t.integer :depth_cache
+      t.string :type
+    end
+
+    create_table :alternative_test_nodes do |t|
+      t.string :alternative_ancestry
+    end
+
+    create_table :other_test_nodes do |t|
+      t.string :ancestry
+    end
+
+    create_table :parent_id_test_nodes do |t|
+      t.string :ancestry
+      t.integer :parent_id
+    end
+
+    create_table :acts_as_tree_test_nodes do |t|
+      t.string :ancestry
+    end
+  end
+
+  $stdout = $stdout_orig
+end
+
+def teardown_db
+  ActiveRecord::Base.connection.tables.each do |table|
+     ActiveRecord::Base.connection.drop_table(table)
+   end
+end
 
 class TestNode < ActiveRecord::Base
-  has_ancestry :cache_depth => true, :depth_cache_column => :depth_cache
 end
 
 class AlternativeTestNode < ActiveRecord::Base
-  has_ancestry :ancestry_column => :alternative_ancestry, :orphan_strategy => :rootify
 end
 
 class ActsAsTreeTestNode < ActiveRecord::Base
-  acts_as_tree
 end
 
 class ParentIdTestNode < ActiveRecord::Base
@@ -23,8 +65,42 @@ end
 class TestNodeSub2 < TestNode
 end
 
+# In order for the `has_ancestry` scopes to make use of the Rails
+# 3.1-compatible `where` method rather than use the old finder options, the
+# scopes must be initialized *after* the database has been created.
+#
+# If `has_ancestry` is declared directly in the classes above before the test
+# database has been created, ActiveRecord will complain and all tests will
+# fail. Hence the reason for the `setup_ancestry` method which can be run
+# after the database columns have been initialized.
+#
+def setup_ancestry
+  TestNode.instance_eval do
+    has_ancestry :cache_depth => true,
+                 :depth_cache_column => :depth_cache
+  end
+
+  AlternativeTestNode.instance_eval do
+    has_ancestry :ancestry_column => :alternative_ancestry,
+                 :orphan_strategy => :rootify
+  end
+
+  ActsAsTreeTestNode.instance_eval do
+    acts_as_tree
+  end
+
+end
+
 class ActsAsTreeTest < ActiveSupport::TestCase
-  load_schema
+
+  def setup
+    setup_db
+    setup_ancestry
+  end
+
+  def teardown
+    teardown_db
+  end
   
   def setup_test_nodes model, level, quantity
     model.delete_all
